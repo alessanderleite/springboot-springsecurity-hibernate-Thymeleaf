58040f31d58302e4f83e769fb4a1f4e6fea95dc0
alessanderleite <alessander.logistica@gmail.com>
2019-06-27 13:29:57 -0300

    create the model classes

---
+++ src/main/java/br/com/alessanderleite/app/model/User.java
@@ -0,0 +1,27 @@
+package br.com.alessanderleite.app.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	private Collection<Rol> roles;
+}
